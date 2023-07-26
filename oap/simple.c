4553164421592f739f69e730d073b790bb4df7ef
Rubin Simons <me@rubin55.org>
2023-07-26 17:09:13 +0300

    First low-level OpenGL program, from OpenGL: A Primer.

---
+++ oap/simple.c
@@ -0,0 +1,33 @@
+#include <GL/glut.h>
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glBegin(GL_POLYGON);
+    glVertex2d(-0.5, -0.5);
+    glVertex2d(-0.5, 0.5);
+    glVertex2d(0.5, 0.5);
+    glVertex2d(0.5, -0.5);
+  glEnd();
+
+  glFlush();
+}
+
+void init() {
+  glClearColor(0.3f, 0.1f, 0.0f, 0.0f);
+  glColor3d(1.0, 0.4, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(0, 0);
+  glutCreateWindow("simple");
+  glutDisplayFunc(&display);
+  init();
+  glutMainLoop();
+}
